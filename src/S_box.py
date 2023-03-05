4ee147d13f7afb7061d70a61dab26e6f926c1b1a
hagli <hagli2001@gmail.com>
2023-03-05 13:17:52 +0000

    Fix S-Box.

--- src/S_box.py
+++ src/S_box.py
@@ -1,7 +1,7 @@
 def create_s_box() :
     # transform integer to its multiplicative inverse in the field
     transform_map = [0, # 0 is mapped to itself, it should not have any multiplicative inverse
-                     1, 9, 14, 13, 11, 7, 6, 15, 4, 12, 5, 10, 4, 3, 8]
+                     1, 9, 14, 13, 11, 7, 6, 15, 2, 12, 5, 10, 4, 3, 8]
     for i in range(len(transform_map)):
         x = transform_map[i]
         x = x^(x << 1)^(x << 2)^(x << 3)^(x << 4)^3
